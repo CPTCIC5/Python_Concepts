798715a96075ec0435d67c3b181fcd03ef46f83c
Aryan <aryanjainak@gmail.com>
2022-04-01 12:24:49 +0530

    updates

--- playing_with_csv.py
+++ playing_with_csv.py
@@ -6,15 +6,16 @@ no=input("Number :")
 with open("phonebook.csv","a") as e:
     writer=csv.writer(e)
     writer.writerow([name,no])
-
 '''
 
+'''
+uniqueness=set()
 with open('phonebook.csv','r') as file:
-    reader=csv.reader(file)
-<<<<<<< HEAD
-    next(reader)
-=======
->>>>>>> test
-    #csv_reader=csv.DictReader(file)
+    #reader=csv.reader(file)
+    #next(reader)
+    reader=csv.DictReader(file)
     for row in reader:
-        print(row)
+        uniqueness.add(row['ARYAN'].upper())
+for unique in sorted(uniqueness):
+    print(unique)
+'''
